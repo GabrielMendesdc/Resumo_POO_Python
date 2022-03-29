a49cf2d3f670567cb2e4fd2ee824e94bed57d5fb
Gabriel Mendes de Campos <87998234+GabrielMendesdc@users.noreply.github.com>
2022-03-29 11:09:41 -0300

    Add files via upload

--- classe_exs.py
+++ classe_exs.py
@@ -2,13 +2,17 @@ lista_de_nomes_usados = ['Bradesco']
 
 class Banco:
     def __init__(self, nome):
+
         # O Underscore indica que o atributo apenas deve ser usado por subclasses, e não fora da classe
         # Se fosse __nome pra acessar seria self._Banco__nome
         self._nome = nome
+
         # Criei esse atributo pra associar outra classe a ele e testar associação de classes
         self.empresas_terceirizadas = []
         self._clientes = []
-        global lista_de_nomes_usados
+
+        # Usei o global pra substituir um banco de dados, uma informação externa a classe pra testar o comportamento
+        global lista_de_nomes_usados  
         lista_de_nomes_usados.append(self._nome)
 
     @property
@@ -36,32 +40,46 @@ class Banco:
         indice = self._clientes.index(cliente)
         self._clientes[indice].entra_dinheiro(valor)
 
+    def gera_cartao(self, cliente, limite):
+        cartao = CartaoDeCredito(cliente, limite)
+        print(f'Cartao de crédito criado com sucesso para {cliente._nome} com R${limite},00 de limite')
+
+    def __del__(self):
+        print(self.nome, 'foi apagado')
+
 # Instanciando um Banco
 santander = Banco('Santander')
 
 # Testando o setter
-st1 = Banco('SantoAndre1')
+st1 = Banco('SantoAndre1ClonadoBangu')
 st1.nome = 'Santander'
 
 
-class Limpeza_terceirizada():
+class LimpezaTerceirizada():
     def __init__(self, empresa_contratante, preco):
         self.empresa_contratante = empresa_contratante
         self.preco = preco
     
     def limpa(self):
-        print(f'Estou limpando o {self.empresa_contratante} por {self.preco}')
+        print(f'Estamos limpando o {self.empresa_contratante} por R${self.preco},00')
+
+    def __del__(self):
+        print('limpeza foi apagado')
 
 # Cria uma empresa de limpeza
-limpa_tudo = Limpeza_terceirizada('Banco Santander ag 0',5000)
+limpa_tudo = LimpezaTerceirizada('Banco Santander' ,5050)
+
 # Associa a nova empresa no banco criado antes
 id = santander.contrata(limpa_tudo)
+
 # Chama o módulo limpa dentro de limpeza terceirizada, associada na lista empresas_terceirizadas (um atributo do banco)
 # Por chamar um método de uma classe através da outra é uma associação de classes, pois ambas são independentes
 santander.empresas_terceirizadas[id].limpa()
 
-# Composição e Agregação
-# Exemplo de agregação
+
+# Exemplo de agregação: A classe Cliente pode existir totalmente sozinha, e a classe Banco também, mas 
+# Várias funções de Banco necessitam de um Cliente, o que torna essa relação uma agregação
+
 class Cliente:
     def __init__(self, nome, saldo=0):
         self._nome = nome
@@ -83,10 +101,37 @@ class Cliente:
                 print(f'Saque de {i}')
         print(f'Saldo atual: {self._Cliente__saldo}')
 
+    def __del__(self):
+        print(f'cliente {self._nome} foi apagado')
+
+
+# Criei essa classe cartão de crédito para demonstrar a relação de composição entre
+# Classes, a instância cartao da classe CartaoDeCredito foi apagada antes mesmo do banco,
+# Pois ela foi instanciada dentro de Banco logo ela compõe este
+
+
+class CartaoDeCredito:
+    def __init__(self, cliente, limite):
+        self.cliente = cliente
+        self._limite = limite
+
+    def __del__(self):
+        print('Cartão de crédito deletado')
+
 
 joao = Cliente('João')
 santander.abre_conta(joao)
 joao.confere()
 santander.deposita(joao, 5000)
 santander.deposita(joao, -4999)
+santander.gera_cartao(joao, 700)
+
+# Quando apagamos a instancia de santander da memória,
+# A limpeza_terceirizada e os cliente não foram apagados 
+# Antes do programa ser executado, como ocorreu com santander
+
+del santander
 joao.extrato()
+
+
+
